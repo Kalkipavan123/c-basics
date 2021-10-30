295b9f5f9759e63734b33f7cbf94001158f9f6d1
Kalkipavan123 <93321963+Kalkipavan123@users.noreply.github.com>
2021-10-29 23:44:17 -0700

    Add files via upload

---
+++ pavan program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable
+	int num1,num2,sum;
+	
+	//intialization
+	num1=10;
+	num2=20;
+	
+	//calculation
+	sum=num1+num2;
+	
+	//out put
+	printf("%d",sum);
+}
